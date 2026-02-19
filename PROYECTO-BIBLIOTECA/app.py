150b302918d636a04ce734ae24864c63c3ef9cf1
Nayeli <nl.ramireza@uea.edu.ec>
2026-02-19 12:30:11 -0500

    PROYECTO-BIBLIOTECA
    PROYECTO-BIBLIOTECA

--- PROYECTO-BIBLIOTECA/app.py
+++ PROYECTO-BIBLIOTECA/app.py
@@ -1,24 +1,29 @@
+from flask import Flask, render_template
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return 'Biblioteca Virtual – Consulta de libros'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/libros')
 def libros():
-    return 'Listado de libros'
+    return render_template('libros.html')
 
 @app.route('/autores')
 def autores():
-    return 'Listado de autores'
+    return render_template('autores.html')
 
 @app.route('/prestamos')
 def prestamos():
-    return 'Listado de préstamos'
+    return render_template('prestamos.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
